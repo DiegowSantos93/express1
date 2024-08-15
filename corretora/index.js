058d1bf95cda7521aa03cc82a30a96afe9011f02
Diego Willian <diegowillian41@gmail.com>
2024-08-14 22:47:43 -0300

    Update index.js

--- corretora/index.js
+++ corretora/index.js
@@ -7,12 +7,12 @@ app.use(express.json())
 
 app.get('/corretora', (req, res) => {
     res.json(corretora.showAll())
-})
+});
 
 app.post('/corretora', (req, res) => {
     const result = corretora.criar(req.body);
     res.status(result.status).json({ message: result.message})
-})
+});
 
 app.put('/corretora/:id', (req, res) => {
     const id = parseInt(req.params.id); // Captura o ID da URL
@@ -28,4 +28,4 @@ app.delete('/corretora/:id', (req, res) => {
 
 app.listen(porta, () => {
     console.log(`Bora lá, porta ${porta}`);
-})
+});
